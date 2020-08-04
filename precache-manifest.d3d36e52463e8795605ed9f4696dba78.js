939245d228ec2fad930800bb222d005309bbf0ed
Khang. Le Nguyen <khangln@vng.com.vn>
2020-08-04 20:53:11 +0700

    Updates

---
+++ precache-manifest.d3d36e52463e8795605ed9f4696dba78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebff348dce42c9f6481406c8179e262",
+    "url": "/ghem-food/index.html"
+  },
+  {
+    "revision": "e230b82468c3ff08fdc9",
+    "url": "/ghem-food/static/css/main.3f227487.chunk.css"
+  },
+  {
+    "revision": "4ed5956a61fd50e00d0e",
+    "url": "/ghem-food/static/js/2.2b824eb6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ghem-food/static/js/2.2b824eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e230b82468c3ff08fdc9",
+    "url": "/ghem-food/static/js/main.b362fac8.chunk.js"
+  },
+  {
+    "revision": "25cb191937f6dfc5613a",
+    "url": "/ghem-food/static/js/runtime-main.b20e5114.js"
+  }
+]);
