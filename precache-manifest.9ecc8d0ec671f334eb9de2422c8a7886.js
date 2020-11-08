bdfbba6b59b47c3dc1061920c589907581303200
Khang Le <khangln@vng.com.vn>
2020-11-08 23:36:14 +0700

    Updates

---
+++ precache-manifest.9ecc8d0ec671f334eb9de2422c8a7886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca4c0704304723774790f3fba289672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9d98c7ee02a801dec2f",
+    "url": "/static/css/main.02696037.chunk.css"
+  },
+  {
+    "revision": "8398896c98d548798a92",
+    "url": "/static/js/2.6df22984.chunk.js"
+  },
+  {
+    "revision": "0dbd69f66c40730063d6e4826301feb4",
+    "url": "/static/js/2.6df22984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d98c7ee02a801dec2f",
+    "url": "/static/js/main.11d95190.chunk.js"
+  },
+  {
+    "revision": "964171fb9c40745cf45b",
+    "url": "/static/js/runtime-main.079581e2.js"
+  }
+]);
